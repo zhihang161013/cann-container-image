c8b6646e89693090ab3d8b16d8bd6c96d7c85c0b
Icey <1790571317@qq.com>
2025-04-21 09:26:49 +0800

    [Feat] Improve Dockerfile template (#7)

--- tools/template.py
+++ tools/template.py
@@ -1,5 +1,8 @@
 import os
+import re
+import requests
 import json
+from distutils.version import LooseVersion
 from jinja2 import Environment, FileSystemLoader
 
 BASE_URL = "https://ascend-repo.obs.cn-east-2.myhuaweicloud.com"
@@ -13,16 +16,35 @@ ALPHA_DICT = {
 
 env = Environment(loader=FileSystemLoader('tools/template'))
 
-def get_download_url(cann_chip, version, nnal_version):
+def get_python_download_url(version):  
+    try:
+        response = requests.get("https://www.python.org/ftp/python/")
+        response.raise_for_status()
+        versions = re.findall(rf"{version}\.[0-9]+", response.text)
+        if not versions:
+            print(f"[WARNING] Could not find the latest version for Python {version}")
+            exit(1)
+        py_latest_version = sorted(versions, key=LooseVersion)[-1]
+        print(f"Latest Python version found: {py_latest_version}")
+    
+    except requests.RequestException as e:
+        print(f"[WARNING] Error fetching Python versions: {e}")
+        exit(1)
+        
+    py_installer_package = "Python-" + py_latest_version
+    py_installer_url = os.path.join("https://repo.huaweicloud.com/python/", py_latest_version, py_installer_package + ".tgz")
+    return py_installer_package, py_installer_url, py_latest_version
+
+def get_cann_download_url(cann_chip, version, nnal_version):
     if "alpha" in version:
         if version not in ALPHA_DICT:
             raise ValueError(f"Unsupported version: {version}. Supported versions are: {list(ALPHA_DICT.keys())}")
-        url_prefix = BASE_URL + "/Milan-ASL/Milan-ASL%20" + ALPHA_DICT[version]
+        url_prefix = f"{BASE_URL}/Milan-ASL/Milan-ASL%20{ALPHA_DICT[version]}"
     else:
-        url_prefix = BASE_URL + "/CANN/CANN%20" + version
-        
-    nnal_url_prefix = BASE_URL + "/CANN/CANN%20" + nnal_version
-        
+        url_prefix = f"{BASE_URL}/CANN/CANN%20{version}"
+    
+    nnal_url_prefix = f"{BASE_URL}/CANN/CANN%20{nnal_version}"
+    
     toolkit_file_prefix = "Ascend-cann-toolkit_" + version + "_linux"
     kernels_file_prefix = "Ascend-cann-kernels-" + cann_chip + "_" + version + "_linux"
     nnal_file_prefix = "Ascend-cann-nnal_" + nnal_version + "_linux"
@@ -30,20 +52,28 @@ def get_download_url(cann_chip, version, nnal_version):
     cann_toolkit_url_prefix = f"{url_prefix}/{toolkit_file_prefix}"
     cann_kernels_url_prefix = f"{url_prefix}/{kernels_file_prefix}"   
     cann_nnal_url_prefix = f"{nnal_url_prefix}/{nnal_file_prefix}"
+    
     return cann_toolkit_url_prefix, cann_kernels_url_prefix, cann_nnal_url_prefix
     
 def render_and_save(template_name, item):
     template = env.get_template(template_name)
-    cann_toolkit_url_prefix, cann_kernels_url_prefix, cann_nnal_url_prefix = get_download_url(
-        item['cann_chip'], 
-        item['cann_version'], 
-        item['nnal_version']
-        )
-    item['cann_toolkit_url_prefix'] = cann_toolkit_url_prefix
-    item['cann_kernels_url_prefix'] = cann_kernels_url_prefix
-    item['cann_nnal_url_prefix'] = cann_nnal_url_prefix
+    
+    py_installer_package, py_installer_url, py_latest_version = get_python_download_url(item["py_version"])
+    item["py_installer_package"] = py_installer_package
+    item["py_installer_url"] = py_installer_url
+    item["py_latest_version"] = py_latest_version
+    
+    cann_toolkit_url_prefix, cann_kernels_url_prefix, cann_nnal_url_prefix = get_cann_download_url(
+        item["cann_chip"], 
+        item["cann_version"], 
+        item["nnal_version"]
+    )
+    item["cann_toolkit_url_prefix"] = cann_toolkit_url_prefix
+    item["cann_kernels_url_prefix"] = cann_kernels_url_prefix
+    item["cann_nnal_url_prefix"] = cann_nnal_url_prefix
+    
     rendered_content = template.render(item=item)
-
+    
     output_path = os.path.join("cann", item['tags']['common'][0], "Dockerfile")
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     with open(output_path, 'w') as f:
